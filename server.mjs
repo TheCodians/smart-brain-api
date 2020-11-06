cd36008bce4271ca0fb8981cb3a5097bde953cf4
PrashantKeshwad <36850676+PrashantKeshwad@users.noreply.github.com>
2020-11-06 16:03:15 +0530

    loading project

---
+++ server.mjs
@@ -0,0 +1,37 @@
+import express from 'express';
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
+import knex from 'knex';
+
+import register from './controllers/register.js';
+import signin from './controllers/signin.js';
+import profile from './controllers/profile.js';
+import image from './controllers/image.js';
+
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'namaskar',
+    database : 'smart-brain'
+  }
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res) => {res.send(db.users)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('app is running on port ${process.env.PORT}');
+});
