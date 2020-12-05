03c1e182b935b70edfd2b1a98ab1f23ab0516192
Prashant Keshwad <prashantkeshwad@gmail.com>
2020-12-05 16:37:31 +0530

    updates

--- server.mjs
+++ server.mjs
@@ -3,8 +3,8 @@ import bcrypt from "bcrypt-nodejs";
 import cors from "cors";
 import knex from "knex";
 
-import register from "./controllers/register.js";
 import signin from "./controllers/signin.js";
+import register from "./controllers/register.js";
 import profile from "./controllers/profile.js";
 import image from "./controllers/image.js";
 
